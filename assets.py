8a2cd5b52fd1537250f012c4b79aba1c2692c50b
DESKTOP-O7DJTHS\Adam <adam.lugowski@live.com>
2023-12-19 14:34:28 +0100

    Added new function to count asset average. Minor changes in syntax

--- assets.py
+++ assets.py
@@ -5,17 +5,25 @@ class Asset:
     # I will try to make this function work on condition:
     # if asset in database: increase quantity and count value using weighted average
     def add_asset(self, name: str, value: float, quantity: float):
-        sql = 'INSERT INTO assets VALUES (null, ?, ?, ?)'
+        money_invested = quantity * value
         cursor = self.connection.cursor()
-        cursor.execute(sql, (name, value, quantity))
+        cursor.execute('INSERT INTO assets VALUES (null, ?, ?, ?, ?)', (name, value, quantity, money_invested))
 
     def show_balance(self, name: str):
         cursor = self.connection.cursor()
-        cursor.execute('SELECT SUM(QUANTITY) FROM assets WHERE name=?', (name, ))
+        cursor.execute('SELECT SUM(quantity) FROM assets WHERE name=?', (name, ))
         result = cursor.fetchone()
-        print(result[0] if result else 0)
+        print(result[0])
 
-    def remove_asset(self, name, quantity):
+    def remove_asset(self, name):
         cursor = self.connection.cursor()
         cursor.execute('DELETE FROM assets WHERE name=?', (name, ))
         self.connection.commit()
+
+    # funkcja do zliczania średniej zakupowej
+    def asset_average(self, name):
+        cursor = self.connection.cursor()
+        cursor.execute('SELECT SUM(money_invested) / SUM(quantity) FROM assets WHERE name=?', (name, ))
+        result = cursor.fetchone()
+        print(result[0])
+
